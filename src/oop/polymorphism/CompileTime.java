98cf612a99ba06538c8d9adc8b857bc18ecc460d
Veli Uysal (BilgeAdam Eğitmen) <veli.uysal@baogrenci.com>
2023-09-09 10:53:16 +0300

    Polymorphism

--- src/oop/polymorphism/CompileTime.java
+++ src/oop/polymorphism/CompileTime.java
@@ -1,12 +1,15 @@
 package oop.polymorphism;
 
 class MathOperations {
-    int add(int a, int b) {
+
+    double add(int a, double b) {
+        System.out.println("1");
         return a + b;
     }
 
-    double add(double a, double b) {
-        return a + b;
+    double add(double a, int b) {
+        System.out.println("2");
+        return (a + b);
     }
 }
 
@@ -14,8 +17,8 @@ public class CompileTime {
 
     public static void main(String[] args) {
         MathOperations ops = new MathOperations();
-        int result1 = ops.add(2, 3);
-        double result2 = ops.add(2.5, 3.7);
+        double result1 = ops.add(2, 3.0);
+        double result2 = ops.add(2.0, 3);
         System.out.println("Result1: " + result1);
         System.out.println("Result2: " + result2);
     }

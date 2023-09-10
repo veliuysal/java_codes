9945a7dccaed9ac4ec1369e225875336add9cf16
Veli Uysal (BilgeAdam Eğitmen) <veli.uysal@baogrenci.com>
2023-09-10 13:59:52 +0300

    Enumerations

--- src/oop/enumeration/WorkingEnum.java
+++ src/oop/enumeration/WorkingEnum.java
@@ -1,15 +1,41 @@
 package oop.enumeration;
 
 enum CollarType {
- WHITE_COLLAR,BLUE_COLLAR
+    WHITE_COLLAR, BLUE_COLLAR, OTHER
+}
+
+enum Legislation {
+    LAW_657, LAW_OTHER
 }
 
 enum PersonalType {
- WORKER, OFFICER, 
+    
+    WORKER(Legislation.LAW_OTHER, CollarType.BLUE_COLLAR),
+    OFFICER(Legislation.LAW_657, CollarType.WHITE_COLLAR),
+    CONTRACT_WORKER(Legislation.LAW_OTHER, CollarType.BLUE_COLLAR),
+    CONTRACT_OFFICER(Legislation.LAW_657, CollarType.WHITE_COLLAR),
+    OTHER(Legislation.LAW_OTHER, CollarType.OTHER);
+
+    private Legislation legislation;
+    private CollarType collarType;
+
+    PersonalType(Legislation legislation, CollarType collarType) {
+        this.collarType = collarType;
+        this.legislation = legislation;
+    }
+
+    public CollarType getCollarType() {
+        return this.collarType;
+    }
+
+    public Legislation getLegislation() {
+        return this.legislation;
+    }
+
 }
 
 public class WorkingEnum {
-  //İşçi, Memur, Sözleşmeli Memur, Sözleşmeli İşçi, Diğer isminde Personel Tipimiz olsun.
- //Bu personeller içinde beyaz yaka, mavi yaka özellikleri barındırsın.
- //Tabi oldukları yasaları da içersin. Bu dediğim özelliklere sahip bir PersonalType Enum'ı yazalım.
+    //İşçi, Memur, Sözleşmeli Memur, Sözleşmeli İşçi, Diğer isminde Personel Tipimiz olsun.
+    //Bu personeller içinde beyaz yaka, mavi yaka özellikleri barındırsın.
+    //Tabi oldukları yasaları da içersin. Bu dediğim özelliklere sahip bir PersonalType Enum'ı yazalım.
 }

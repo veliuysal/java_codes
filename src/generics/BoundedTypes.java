87a94593db980f524e7892042bcc3cc82058c245
Veli Uysal (BilgeAdam Eğitmen) <veli.uysal@baogrenci.com>
2023-09-23 12:41:02 +0300

    Generics

--- src/generics/BoundedTypes.java
+++ src/generics/BoundedTypes.java
@@ -1,4 +1,27 @@
 package generics;
 
-public class BoundedTypes {
+public class BoundedTypes<T extends Object> {
+
+    private T data;
+
+    public static void main(String[] args) {
+        BoundedTypes<Integer> intType = new BoundedTypes<>();
+        intType.setData(25);
+
+        BoundedTypes<String> strType = new BoundedTypes<>();
+        strType.setData("Java Dersi");
+
+        System.out.println("Int Type: " + intType.data);
+        System.out.println("Str Type: " + strType.data);
+
+        GenericsClass<Integer> genericsClass = new GenericsClass<>();
+        genericsClass.setItem(23);
+        BoundedTypes<GenericsClass<Integer>> innerGenerics = new BoundedTypes<>();
+        innerGenerics.setData(genericsClass);
+        System.out.println("Inner Generics' Data: " + innerGenerics.data.getItem());
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
 }

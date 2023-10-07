a0722e4fc478739ee4cf9671fc6b5d24c327cd97
Veli Uysal (BilgeAdam Eğitmen) <veli.uysal@baogrenci.com>
2023-10-07 13:55:38 +0300

    Streams

--- src/java8/parallels/SimpleExample.java
+++ src/java8/parallels/SimpleExample.java
@@ -20,12 +20,12 @@ public class SimpleExample {
         LocalDateTime finishTime= LocalDateTime.now();
 
         Stream<Integer> stream2 = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9).stream();
+
+        LocalDateTime startingTime2= LocalDateTime.now();
         System.out.println(stream2.count());
 
         LocalDateTime finishTime2= LocalDateTime.now();
 
-        System.out.println("Starting time: "+startingTime+" finish Time: "+finishTime+" finish Time 2 : "+finishTime2);
-        //System.out.println("Total Time Difference: "+ (finishTime.getNano() - startingTime.getNano()));
-
+        System.out.println("Starting time: "+startingTime+" finish Time: "+finishTime+" Starting Time 2: "+startingTime2+" finish Time 2 : "+finishTime2);
     }
 }
